6e4694de45cb61d21eaa0794a1a7198d21ccbe6a
Andrey Moskalets <voxport@gmail.com>
2023-06-04 17:44:14 +0300

    temp end animation?

--- js/ticker.js
+++ js/ticker.js
@@ -11,19 +11,20 @@
 
 
 window.addEventListener('DOMContentLoaded', function() {
-  var ticker1 = document.querySelector('.ticker1__list--1');
-  var ticker2 = document.querySelector('.ticker2__list--2');
-  var items1 = ticker1.querySelectorAll('.ticker1__item');
-  var items2 = ticker2.querySelectorAll('.ticker2__item');
+  const ticker1 = document.querySelector('.ticker1__list--1');
+  const ticker2 = document.querySelector('.ticker2__list--2');
+  const items1 = ticker1.querySelectorAll('.ticker1__item');
+  const items2 = ticker2.querySelectorAll('.ticker2__item');
 
   // Копируем элементы списка для создания эффекта зацикленности
-  for (var i = 0; i < items1.length; i++) {
-    var clone1 = items1[i].cloneNode(true);
+  for (let i = 0; i < items1.length; i++) {
+    const clone1 = items1[i].cloneNode(true);
     ticker1.appendChild(clone1);
   }
 
-  for (var i = 0; i < items2.length; i++) {
-    var clone2 = items2[i].cloneNode(true);
+  for (let i = 0; i < items2.length; i++) {
+    const clone2 = items2[i].cloneNode(true);
     ticker2.appendChild(clone2);
   }
 });
+
